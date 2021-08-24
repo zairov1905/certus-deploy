80d58de79353d22b696a0a4e6aca51e7e9d5aa15
zairov1905 <68842891+zairov1905@users.noreply.github.com>
2021-08-24 17:41:37 +0400

    Last changes is done

--- src/features/dashboard/settings/skill/skillActions.js
+++ src/features/dashboard/settings/skill/skillActions.js
@@ -1,14 +1,11 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import { fetchSampleDataSkill } from "../../../../app/api/mockApi";
-
 import {
   APP_LOADED,
   asyncActionError,
   asyncActionFinish,
   asyncActionStart,
 } from "../../../../app/async/asyncReducer";
-import { delay } from "../../../../app/util/util";
 import {
   CREATE_SKILL,
   DELETE_SKILL,
@@ -20,20 +17,22 @@ const url = "skill";
 export function loadSkill(data) {
   return async function (dispatch) {
     dispatch(asyncActionStart());
-
-    const references = await axios.get(`/${url}`, {
-      params: { ...data },
-    });
-    if (references.status === 200) {
-      dispatch({
-        type: FETCH_SKILL,
-        payload: references.data.data,
-        totalCount: references.data.message,
+    await axios
+      .get(`/${url}`, {
+        params: { ...data },
+      })
+      .then((datas) => {
+        dispatch({
+          type: FETCH_SKILL,
+          payload: datas.data.data,
+          totalCount: datas.data.message,
+        });
+        dispatch(asyncActionFinish());
+      })
+      .catch((err) => {
+        dispatch(asyncActionError(err.message));
+        toast.info("Xəta baş verdi");
       });
-      dispatch(asyncActionFinish());
-    } else {
-      dispatch(asyncActionError());
-    }
   };
 }
 export function listenToSkill(skills) {
@@ -46,51 +45,54 @@ export function listenToSkill(skills) {
 export function createSkill(skill) {
   return async function (dispatch) {
     dispatch(asyncActionStart());
-    const data = await axios.post(`${url}/create`, skill, {
-      withCredentials: true,
-    });
-    if (data.status === 201) {
-      toast.success("Uğurla əlavə edildi");
-      dispatch({ type: CREATE_SKILL, payload: data.data.data });
-      dispatch(asyncActionFinish());
-    } else {
-      toast.danger("Xəta baş verdi, yenidən cəht edin.");
-    }
+    await axios
+      .post(`${url}/create`, skill, {
+        withCredentials: true,
+      })
+      .then((data) => {
+        dispatch({ type: CREATE_SKILL, payload: data.data.data });
+        dispatch(asyncActionFinish());
+        toast.success("Uğurla əlavə edildi");
+      })
+      .catch((err) => {
+        dispatch(asyncActionError(err.message));
+        toast.info("Xəta baş verdi, yenidən cəht edin.");
+      });
   };
 }
 
 export function updateSkill(skill) {
   return async function (dispatch) {
     dispatch(asyncActionStart);
-
-    const skillUpdated = await axios.put(
-      `/${url}/update`,
-      skill
-    );
-    if (skillUpdated.status === 200) {
-      toast.success("Dəyişiklik uğurlar yerinə yetirildi");
-      dispatch({
-        type: UPDATE_SKILL,
-        payload: skillUpdated.data.data,
+    await axios
+      .put(`/${url}/update`, skill)
+      .then((data) => {
+        dispatch({
+          type: UPDATE_SKILL,
+          payload: data.data.data,
+        });
+        dispatch(asyncActionFinish());
+        toast.success("Dəyişiklik uğurlar yerinə yetirildi");
+      })
+      .catch((err) => {
+        dispatch(asyncActionError(err.message));
+        toast.info("Xəta baş verdi, yenidən cəht edin.");
       });
-      dispatch(asyncActionFinish());
-    } else {
-      asyncActionError();
-    }
   };
 }
 
 export function deleteSkill(skillId) {
   return async function (dispatch) {
-    const documentDeleted = await axios.delete(
-      `/${url}/delete?id=${skillId}`
-    );
-    if (documentDeleted.status === 200) {
-      dispatch({ type: DELETE_SKILL, payload: skillId });
-      // dispatch(asyncActionFinish())
-      toast.info("Uğurla silindi");
-    } else {
-      dispatch(asyncActionError());
-    }
+    await axios
+      .delete(`/${url}/delete?id=${skillId}`)
+      .then((data) => {
+        dispatch({ type: DELETE_SKILL, payload: skillId });
+        dispatch(asyncActionFinish());
+        toast.info("Uğurla silindi");
+      })
+      .catch((err) => {
+        dispatch(asyncActionError(err.message));
+        toast.info("Xəta baş verdi, yenidən cəht edin.");
+      });
   };
 }
